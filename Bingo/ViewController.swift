80fdda8568397d9a156ea6ceb61e4bd6297068de
Ying_Chen <emily1017c@gmail.com>
2019-10-25 18:09:55 +0800

    Initial Commit

---
+++ Bingo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bingo
+//
+//  Created by 陳穎 on 2019/10/25.
+//  Copyright © 2019 陳穎. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
