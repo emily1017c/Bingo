23673fb6713846a8de9ac62e15fff7736ce535b0
Ying_Chen <emily1017c@gmail.com>
2019-11-08 16:09:03 +0800

    login

--- Bingo/ViewController.swift
+++ Bingo/ViewController.swift
@@ -15,6 +15,24 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func showMessage(sender: UIButton) {
+        let selectedButton = sender
+        
+        // 從所選按鈕的標題標籤取得表情符號
+        if let wordToLookup = selectedButton.titleLabel?.text {
+            
+            // 從字典取得表情符號的意義
+            // 程式碼填入至下方
+            
+            
+            
+            // 變更以下這行程式將 Hello World 的顯示以來表情符號的意義來取代
+            let alertController = UIAlertController(title: wordToLookup, message:"登入成功" , preferredStyle: UIAlertController.Style.alert)
+            
+            alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+            present(alertController, animated: true, completion: nil)
+        }
+    }
 
 }
 
